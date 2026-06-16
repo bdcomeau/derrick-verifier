1fc8b6bb16fc55b37d9ee1829cc0d954170ac790
bdcomeau <33998692+bdcomeau@users.noreply.github.com>
2026-06-16 01:04:12 -0600

    Add files via upload

---
+++ derrick-verifier-sw.js
@@ -0,0 +1,6 @@
+const CACHE = 'dgv-v1.0';
+const URLS  = ['./', './index.html'];
+
+self.addEventListener('install',  e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(URLS))));
+self.addEventListener('activate', e => e.waitUntil(caches.keys().then(ks => Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
+self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
